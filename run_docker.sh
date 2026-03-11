c52ec6df700beb4c5f48d5e2f94c9efa8f4bf4a9
joe <jb5@vt.edu>
2026-03-11 12:51:45 -0400

    docker image works

---
+++ run_docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Build the Docker image
+#docker build -t navngen -f docker/Dockerfile .
+
+DOCKER_ARGS=""
+
+#DOCKER_ARGS+="-v /home/joe/vt/LightGlue/"
+DOCKER_ARGS+="-v /home/joe/vt/research/glue/data/airport/cropped_images:/data/airport/"
+# Run the Docker container
+# This will start an interactive session inside the container.
+# You can then run your python scripts.
+docker run -it --rm \
+    -v "$(pwd)/src:/app/src" \
+    $DOCKER_ARGS \
+    navngen
